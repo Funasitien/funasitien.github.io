c70924dfc68bb643071fe11369d9a7ad32be115b
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2023-11-09 20:43:31 +0100

    🎨 IMAGE BOMB

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}", "./*.{html,js}", "./archive/kernacraft/*"],
+  content: ["./*/*.{html,js}", "./*.{html,js}", "./*/kernacraft/*"],
   theme: {
     extend: {},
   },
