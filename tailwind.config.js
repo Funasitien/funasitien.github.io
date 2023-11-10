27d93b87f2712b425a477ce4dcf994294423c356
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2023-11-10 20:56:09 +0100

    ✨ Added WEB ARCHIVE

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*/*.{html,js}", "./*.{html,js}", "./*/kernacraft/*"],
+  content: ["./archive/*.{html,js}", "./archive/*/*.{html,js}"],
   theme: {
     extend: {},
   },
