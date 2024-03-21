169b18b25ceac1f82107c5c924e5d5a442434a33
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-03-21 17:17:29 +0100

    🤌 Maj

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
