0e5fe520fb4319bd90e5d1f9f4add6045720ddfc
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-05-31 08:12:22 +0200

    📝 Add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx()],
   site: 'https://f.dreamclouds.fr',
+  integrations: [tailwind(), icon(), mdx(), sitemap()],
 });
