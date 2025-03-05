b2396584008200d28ada2c2c21a24155aed8c2f4
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2025-03-05 07:55:01 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://f.dreamclouds.fr',
+  site: 'futur.funacode.pages.dev',
   integrations: [tailwindcss(), icon(), mdx(), sitemap()],
   prefetch: {
     prefetchAll: true
