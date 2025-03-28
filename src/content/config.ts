404bed6affaeaaccae495c97122921138f244142
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2025-03-28 08:58:01 +0000

    🔥 Test Doc intégré

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+
+const docsCollection = defineCollection({
+  schema: z.object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+    draft: z.boolean().optional(),
+    order: z.number().optional(),
+  }),
+});
+
+export const collections = {
+  docs: docsCollection,
+};
